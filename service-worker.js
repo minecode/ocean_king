7b59cf5d4f00b90d89e832b9ab9f901ae4c5c3f7
minecodebot <minecode.geral@gmail.com>
2020-03-30 18:17:41 +0000

    deploy: 80dca7fe2b576d67e2a0e6a5bd1bc8034731a834

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ocean_king/precache-manifest.d5f7508a6cb425b81e7cfc39a362d0fc.js"
+  "/ocean_king/precache-manifest.67e7b10899df9838a57b27d84cf93f94.js"
 );
 
 workbox.skipWaiting();
